52679f826ac3836b120efb310e1126a6992fd9c3
leyew <102467346+itsme-zeix@users.noreply.github.com>
2025-12-02 20:59:55 +0800

    add parallelized brute force variant to 2025/day02

--- 2025/day02/day02.py
+++ 2025/day02/day02.py
@@ -1,12 +1,16 @@
 from typing import Optional
 from time import perf_counter_ns
 
+# For parallelization
+import os
+from concurrent.futures import ProcessPoolExecutor
+
 def timeit(f):
     def wrap(*args, **kwargs):
         start = perf_counter_ns()
         res = f(*args, **kwargs)
         end = perf_counter_ns()
-        print(f"Function {f.__name__} took {(end - start)/1000000:.2f}ms")
+        print(f"Function {f.__name__} took {(end - start)/1e6:.2f}ms")
         return res
     return wrap
 
@@ -35,7 +39,7 @@ def part1(input: list[tuple[int, int]]) -> int:
         for id_int in range(l, r + 1):
             id_str = str(id_int)
             id_len = len(id_str)
-            
+
             # odd no. of digits is always valid
             if id_len % 2 != 0:
                 continue 
@@ -44,7 +48,6 @@ def part1(input: list[tuple[int, int]]) -> int:
             if is_invalid(id_str, id_len, substr_len):
                 res += id_int
                 continue
-            
     return res
 
 @timeit
@@ -66,13 +69,65 @@ def part2(input: list[tuple[int, int]]) -> int:
                 if is_invalid(id_str, id_len, substr_len):
                     res += id_int
                     break
-            
     return res
+
+
+# === Parallelized ===
+def part1_par_worker(l: int, r: int) -> int:
+    local_sum = 0
+    for id_int in range(l, r + 1):
+        id_str = str(id_int)
+        id_len = len(id_str)
+
+        # odd no. of digits is always valid
+        if id_len % 2 != 0:
+            continue
+
+        substr_len = id_len // 2
+        if is_invalid(id_str, id_len, substr_len):
+            local_sum += id_int
+    return local_sum
+
+def part2_par_worker(l: int, r: int) -> int:
+    local_sum = 0
+    for id_int in range(l, r + 1):
+        id_str = str(id_int)
+        id_len = len(id_str)
+
+        max_substr_len = id_len // 2
+        for substr_len in range(max_substr_len, 0, -1):
+            if id_len % substr_len != 0:
+                continue
+            if is_invalid(id_str, id_len, substr_len):
+                local_sum += id_int
+                break
+    return local_sum
+
+@timeit
+def part1_par(input: list[tuple[int, int]]) -> int:
+    num_workers = os.cpu_count() or 1
+    with ProcessPoolExecutor(max_workers=num_workers) as executor:
+        # should probably load balance the work as some ranges can be larger than others
+        futures = [executor.submit(part1_par_worker, l, r) for (l, r) in input]
+        return sum(f.result() for f in futures)
+
+@timeit
+def part2_par(input: list[tuple[int, int]]) -> int:
+    num_workers = os.cpu_count() or 1
+    with ProcessPoolExecutor(max_workers=num_workers) as executor:
+        # should probably load balance the work as some ranges can be larger than others
+        futures = [executor.submit(part2_par_worker, l, r) for (l, r) in input]
+        return sum(f.result() for f in futures)
+
 
 if __name__ == "__main__":
     input = read_input("input.txt")
     assert(input)
 
+    # Sequential
     print(part1(input))
     print(part2(input))
 
+    # Parallel
+    print(part1_par(input))
+    print(part2_par(input))
